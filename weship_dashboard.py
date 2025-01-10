85b97521150c28681110e0be32da717caa89f1b1
Jashan Kaleka <57084362+Jashandeep-Singh-Kaleka@users.noreply.github.com>
2025-01-10 10:45:30 -0500

    Add files via upload

--- weship_dashboard.py
+++ weship_dashboard.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from transformers import pipeline
 from sklearn.linear_model import LinearRegression
 import numpy as np
 from prophet import Prophet
